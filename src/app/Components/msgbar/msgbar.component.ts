763043a0ca6539039282f23d91972e4f65d62bf1
HadjerBencheikh <70335101+HadjerBencheikh@users.noreply.github.com>
2022-04-15 12:34:11 +0100

    Add files via upload

---
+++ src/app/Components/msgbar/msgbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-msgbar',
+  templateUrl: './msgbar.component.html',
+  styleUrls: ['./msgbar.component.css']
+})
+export class MsgbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
