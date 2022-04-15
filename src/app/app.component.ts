46b9730ceefc5d6e522b8fe165e7d2f76701b06b
HadjerBencheikh <70335101+HadjerBencheikh@users.noreply.github.com>
2022-04-15 12:46:46 +0100

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,ViewEncapsulation } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css'],
+  encapsulation:ViewEncapsulation.None
 })
+
 export class AppComponent {
-  title = 'angular-typescript-app';
+  title = 'Education';
+  showFiller = false;
+  num=5;
+  hidden = false;
+  panelOpenState = false;
+  toggleBadgeVisibility() {
+    this.hidden = !this.hidden;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MatBadgeModule } from '@angular/material/badge';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TitCardComponent } from './Components/tit-card/tit-card.component';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { SidNavComponent } from './Components/sid-nav/sid-nav.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatTableModule} from '@angular/material/table';
+import { MsgbarComponent } from './Components/msgbar/msgbar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    TitCardComponent,
+    SidNavComponent,
+    MsgbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatBadgeModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatButtonToggleModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatTabsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
